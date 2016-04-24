43429f203a02169efbe716fa49e9c8f1d74e61af
gjulivan <grandjulivan@gmail.com>
2016-04-25 03:52:52 +0700

    update: firebase integration, azure deploy, more css

---
+++ SaleStock.GJulivan.StarWars/obj/Release/Package/PackageTmp/module/main/controller/detailcontroller.js
@@ -0,0 +1,32 @@
+﻿var detailcontroller = function ($scope, hubservice, dataservice) {
+    
+    $scope.CurrentData = dataservice.GetSelectedData();
+    $scope.PropertyAttr = dataservice.GetPropertyAttributes($scope.CurrentData);
+
+    $scope.IsArray = function (data) {
+        return (data.constructor == Array);
+    }
+
+    $scope.LoadData = function (data) {
+        hubservice.RequestDataAsync(data, eventEnum.RequestDetailChanged);
+    }
+
+    $scope.$on(eventEnum.ProcessRequestDetailChanged, function (event, args) {
+        //Clear Previous Data
+        $scope.$apply(function () {
+            $scope.CurrentData = {};
+            $scope.PropertyAttr = [];
+        });
+        //set new data
+        $scope.$apply(function () {
+            $scope.CurrentData = dataservice.GetSelectedData();
+            $scope.PropertyAttr = dataservice.GetPropertyAttributes($scope.CurrentData);
+        });
+        
+    });
+
+    
+
+
+
+}
